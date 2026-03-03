de6ddc599250d5df0bbbd2865871bb7f341f7d40
Svitsa Oleksandr <olexander088@gmail.com>
2026-03-03 20:38:34 +0200

    add TraceEndpoint test

--- log/log_test.go
+++ log/log_test.go
@@ -58,3 +58,56 @@ func TestEndpoint(t *testing.T) {
 		Endpoint(rw, nil)
 	}
 }
+
+
+func TestTraceEndpoint(t *testing.T) {
+	content := "body content"
+	testCases := []struct {
+		read         func() (string, error)
+		writeHeader  func(int)
+		write        func([]byte) (int, error)
+		expectedCode int
+		expectedContent string
+	}{
+		{
+			read: func() (string, error) {
+				return "", errors.New("error")
+			},
+			writeHeader: func(code int) {
+				test.Compare(t, "code", code, http.StatusOK)
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			read: func() (string, error) {
+				return content, nil
+			},
+			writeHeader: func(code int) {
+				test.Compare(t, "code", code, http.StatusOK)
+			},
+			expectedCode: http.StatusOK,
+			write: func(b []byte) (int, error) {
+				test.Compare(t, "content", string(b), content)
+				return 0, nil
+			},
+			expectedContent: content,
+		},
+	}
+
+	for _, testCase := range testCases {
+		rw := &test.MockHttpResposeWriter{
+			MockHeader: func() http.Header {
+				m := make(map[string][]string)
+				return http.Header(m)
+			},
+			MockWriteHeader: testCase.writeHeader,
+			MockWrite:       testCase.write,
+		}
+		l = &logger{
+			traceFile: &MockFile{
+				mockRead: testCase.read,
+			},
+		}
+		TraceEndpoint(rw, nil)
+	}
+}
